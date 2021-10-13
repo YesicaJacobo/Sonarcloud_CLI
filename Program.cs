bf148395c4b5975e6fed54da55a119aad469ed0a
yesicajv <yjv15.yjv@gmail.com>
2021-10-13 16:42:28 -0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Sonarcloud_CLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double num1, num2, resta;
+            
+            Console.WriteLine("--------- Restemos dos numeros! --------- \n"+   
+                              "Introduce el primer numero: \n");
+            num1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Introduce el segundo numero:  \n");
+            num2 = Convert.ToDouble(Console.ReadLine());
+            
+            resta = num1 - num2;
+
+            Console.WriteLine("El resultado de la resta es: "+resta);
+
+        }
+    }
+}
